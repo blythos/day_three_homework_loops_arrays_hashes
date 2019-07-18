3c2a18b527c4ee989ac677043390666f528d848e
Stephen Blythe <stephenblythe@gmail.com>
2019-07-18 09:17:15 +0100

    Adds a return where needed.

--- exercise_b_users.rb
+++ exercise_b_users.rb
@@ -87,6 +87,8 @@ for number in avrils_numbers
   end
 end
 
+return even_numbers
+
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
 
 users["Erik"][:lottery_numbers].push(7)
